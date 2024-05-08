1af6ef1e686b72b6c611e6276f7e7e6f8ca77bda
bbevis <burintbevis@hotmail.com>
2024-05-08 21:19:09 +0100

    xxxx

--- Python/audio.py
+++ Python/audio.py
@@ -58,3 +58,19 @@ beat_chroma = librosa.util.sync(chromagram,
 # Finally, stack all beat-synchronous features together
 beat_features = np.vstack([beat_chroma, beat_mfcc_delta])
 
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True)
+D = librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max)
+img = librosa.display.specshow(D, y_axis='linear', x_axis='time',
+                               sr=sr, ax=ax[0])
+ax[0].set(title='Linear-frequency power spectrogram')
+ax[0].label_outer()
+D = librosa.amplitude_to_db(np.abs(librosa.stft(y, hop_length=hop_length)),
+                            ref=np.max)
+librosa.display.specshow(D, y_axis='log', sr=sr, hop_length=hop_length,
+                         x_axis='time', ax=ax[1])
+ax[1].set(title='Log-frequency power spectrogram')
+ax[1].label_outer()
+fig.colorbar(img, ax=ax, format="%+2.f dB")
+plt.show()
