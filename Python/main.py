f71683fe19f780c462609fefa9707f1bdd54c3c5
bbevis <burintbevis@hotmail.com>
2024-08-31 18:54:40 +0100

    xxxx

--- Python/main.py
+++ Python/main.py
@@ -44,10 +44,12 @@ def get_file_list(lower_threshold, upper_threshold, datatype, outpath):
                                 (metadata['file_size'] <= upper_threshold) & 
                                 (metadata['file_size'] >= lower_threshold) &
                                 (metadata['issues'] == '')].to_list()
+
     
-    # Remove from list audiofiles where visual features already extracted
+    # Remove from list files where features already extracted
     excluded_fs = os.listdir(outpath)
     excluded_fs = [f.replace('.csv', '') for f in excluded_fs]
+    
     fs = [f for f in fs if f not in excluded_fs]
     
     return fs
@@ -68,12 +70,13 @@ def extract_raw_features(data_dir, data_type, type_filenames, outpath):
                         vf = visual.visual_features(file_path,
                                     'Pretrained_models/face_landmarker_v2_with_blendshapes.task',
                                     'Pretrained_models/gesture_recognizer.task')
-                        
+
                     elif data_type == 'vocal':
                         
                         vf = vocal.vocal_features(file_path)
                         
                     resAll = vf.raw_outputs()
+                        
                     resAll.to_csv(outpath + filename + '.csv', index=False)
                     print(f"{filename} has been saved successfully.")
                     
@@ -101,16 +104,16 @@ if __name__ == '__main__':
     # metadata.to_csv('./Output/super_May22/files_metadata.csv', index=False)
 
     ############ video features ############################
-    # video_filenames = get_file_list(200, 400, 'video','Output/super_May22/Video/')
-    # print(video_filenames)
+    video_filenames = get_file_list(1, 500, 'video','Output/super_May22/Video/')
+    print(video_filenames)
 
-    # extract_raw_features(data_directory, 'visual', video_filenames, 'Output/super_May22/Video/')
+    extract_raw_features(data_directory, 'video', video_filenames, 'Output/super_May22/Video/')
     
     ############ vocal features ############################
-    vocal_filenames = get_file_list(0, 10, 'vocal','Output/super_May22/Vocal/')
-    print(vocal_filenames)
+    # vocal_filenames = get_file_list(0, 10, 'vocal','Output/super_May22/Vocal/')
+    # print(vocal_filenames)
 
-    extract_raw_features(data_directory, 'vocal', vocal_filenames, 'Output/super_May22/Vocal/')
+    # extract_raw_features(data_directory, 'vocal', vocal_filenames, 'Output/super_May22/Vocal/')
     
     ############ run time ############################
     end_time = time.time()
