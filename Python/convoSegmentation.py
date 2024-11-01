eff9cd44c92a1cade30fb6c8ecc6110bf652f615
bbevis <burintbevis@hotmail.com>
2024-11-01 14:03:34 +0000

    xxxx

--- Python/convoSegmentation.py
+++ Python/convoSegmentation.py
@@ -29,7 +29,7 @@ logging.info("Reading input data file.")
 data = pd.read_csv(input_file_path)
 
 # Define a set of common backchannel phrases
-backchannel_phrases = {"yes", "yeah", "okay", "uh-huh", "mm-hmm", "mh", "right"}
+# backchannel_phrases = {"yes", "yeah", "okay", "uh-huh", "mm-hmm", "mh", "right"}
 
 # Sort the data by 'Start Time' to ensure sequential analysis
 data = data.sort_values(by='Start Time').reset_index(drop=True)
@@ -49,24 +49,25 @@ logging.info("Processing each row to assign turns, backchannel, and overlap indi
 # Process each row to determine turns, backchannel, and overlap
 for i, row in data.iterrows():
     # Check if the word is a potential backchannel phrase
-    is_backchannel_word = row['Word'].lower() in backchannel_phrases
+    # is_backchannel_word = row['Word'].lower() in backchannel_phrases
     
     # Check if it starts before the previous word has ended (indicating overlap or backchannel)
     is_overlap = row['Start Time'] < prev_end_time and row['Speaker'] != prev_speaker
     
     # Classify as backchannel if it's a backchannel word and overlaps with the previous speaker's speech
-    if is_overlap and is_backchannel_word:
-        data.at[i, 'Backchannel'] = 1
-        data.at[i, 'Turn'] = current_turn  # Keep the turn number the same
+    # if is_overlap and is_backchannel_word:
+    #     data.at[i, 'Backchannel'] = 1
+    #     data.at[i, 'Turn'] = current_turn  # Keep the turn number the same
     # Classify as backchannel if surrounding speakers are different speakers,
     # and start_time_diff is > 10 showing infrequent interjection
     # and while next start time diff is < 5 showing that the previous speaker was still going
-    elif data.at[i, 'start_time_diff'] > 10 and \
+    if data.at[i, 'start_time_diff'] > 10 and \
         data.at[i+1, 'start_time_diff'] < 5 and \
         data.at[i, 'Speaker'] != (data.at[i-1, 'Speaker'] and data.at[i+1, 'Speaker']):
             data.at[i, 'Backchannel'] = 1
             data.at[i, 'Turn'] = current_turn        
-    elif is_overlap and not is_backchannel_word:
+    # elif is_overlap and not is_backchannel_word:
+    elif is_overlap:
         # Otherwise, classify as overlap if it's not a backchannel word
         data.at[i, 'Overlap'] = 1
         data.at[i, 'Turn'] = f"Overlap_{current_turn}"  # Keep the turn number the same
