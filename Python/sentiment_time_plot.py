bbf6aca4faa4cc160006b1e39e58c3c4a591f325
bbevis <burintbevis@hotmail.com>
2024-11-17 15:39:51 +0000

    xxxx

--- Python/sentiment_time_plot.py
+++ Python/sentiment_time_plot.py
@@ -5,36 +5,50 @@ import pandas as pd
 import numpy as np
 import matplotlib.colors as mcolors
 import matplotlib.cm as cm
+from scipy.interpolate import interp1d
 
-def plot_sentiment_time_series(turn_data):
+def plot_sentiment_time_series(word_level_data):
     
     # Create a binary column for "Contest" (1 if contested, 0 if uncontested)
-    turn_data['is_contested'] = turn_data['Contest'].apply(lambda x: 1 if x == 'contested' else 0)
-    
-    turn_data['Duration'] = turn_data['End Time'] - turn_data['Start Time']
+    word_level_data['is_contested'] = word_level_data['Contest'].apply(lambda x: 1 if x == 'contested' else 0)
+    word_level_data['Duration'] = word_level_data['End Time'] - word_level_data['Start Time']
 
     # Group by 'Speaker' and 'Turn' and perform aggregations
-    turn_data = turn_data.groupby(['Speaker', 'Turn']).agg(
+    turn_speaker_data = turn_data.groupby(['Speaker', 'Turn']).agg(
         start_time=('Start Time', 'min'),
         end_time=('End Time', 'max'),
         avg_word_count=('word_count', 'mean'),
         avg_sentiment=('Sentiment', 'mean'),
         count_backchannels=('Backchannel', 'sum'),
-        count_overlaps=('Overlap', 'sum'),
-        prop_contested=('is_contested', 'mean'),
+        mean_overlaps=('Overlap', 'mean'),
+        mean_contested=('is_contested', 'mean'),
         avg_duration=('Duration', 'mean')
     ).reset_index()
 
+    turn_speaker_data['midTime'] = (turn_speaker_data['start_time'] + turn_speaker_data['end_time']) / 2
+    # turn_speaker_data['Turn_duration'] = turn_speaker_data['end_time'] - turn_speaker_data['start_time']
+    turn_speaker_data['Turn_duration'] = (turn_speaker_data['start_time'] + turn_speaker_data['end_time']) / turn_speaker_data['midTime']
+    turn_speaker_data = turn_speaker_data.sort_values(by='midTime')
     
-    turn_data['Time'] = (turn_data['start_time'] + turn_data['end_time']) / 2
-    
-    turn_data = turn_data.sort_values(by='Time')
+    # Group by 'Turn' and perform aggregations
+    turn_level_data = turn_data.groupby(['Turn']).agg(
+        start_time=('Start Time', 'min'),
+        end_time=('End Time', 'max'),
+        avg_word_count=('word_count', 'mean'),
+        avg_sentiment=('Sentiment', 'mean'),
+        count_backchannels=('Backchannel', 'sum'),
+        mean_overlaps=('Overlap', 'mean'),
+        mean_contested=('is_contested', 'mean'),
+        avg_duration=('Duration', 'mean')
+    ).reset_index()
+
+    turn_level_data['Time'] = (turn_level_data['start_time'] + turn_level_data['end_time']) / 2
+    turn_level_data = turn_level_data.sort_values(by='Time')
 
 
     # Prepare the DataFrame by calculating midpoint times for each turn
-    turn_data_A = turn_data[turn_data['Speaker'] == 'A']
-    turn_data_B = turn_data[turn_data['Speaker'] == 'B']
-    
+    turn_speaker_data_A = turn_speaker_data[turn_speaker_data['Speaker'] == 'A']
+    turn_speaker_data_B = turn_speaker_data[turn_speaker_data['Speaker'] == 'B']
 
     # Initialize a figure
     fig, (ax1, ax2) = plt.subplots(2, 1, layout='constrained', figsize=(12, 8))
@@ -47,8 +61,8 @@ def plot_sentiment_time_series(turn_data):
     
 
     ax1.bar(
-        turn_data_A['Turn'], 
-        turn_data_A['avg_word_count'],
+        turn_speaker_data_A['Turn'], 
+        turn_speaker_data_A['avg_word_count'],
         align = 'center',
         width = 1,
         edgecolor='none',
@@ -59,8 +73,8 @@ def plot_sentiment_time_series(turn_data):
     
 
     ax1.bar(
-        turn_data_B['Turn'], 
-        turn_data_B['avg_word_count'],
+        turn_speaker_data_B['Turn'], 
+        turn_speaker_data_B['avg_word_count'],
         align = 'center',
         width = 1,
         edgecolor='none',
@@ -69,31 +83,36 @@ def plot_sentiment_time_series(turn_data):
         alpha = .6
     )
     
-       
+    x_new = np.linspace(turn_level_data['Turn'].min(), turn_level_data['Turn'].max(),500)
+    f = interp1d(turn_level_data['Turn'], turn_level_data['avg_sentiment'], kind='quadratic')
+    y_smooth=f(x_new)
     
-    ax2.plot(turn_data['Turn'],
-                turn_data['avg_sentiment'],
-                label="Sentiment",
-                color='#4C1F7A')
+    ax2.plot(x_new,
+             y_smooth,
+             label="Sentiment",
+             color='#4C1F7A',
+             linewidth=2)
     
-    ax2.fill_between(turn_data['Turn'], turn_data['prop_contested'],
+    ax3 = ax2.twinx()
+    
+    ax2.fill_between(turn_level_data['Turn'], turn_level_data['mean_contested'],
              label='Contested',
              alpha=0.3, step='mid', hatch='//', color = '#db1f48', facecolor = '#db1f48')
     
-    mask_Back = turn_data['count_backchannels'] != 0
+    mask_Back = turn_level_data['count_backchannels'] != 0
     
-    ax2.scatter(turn_data['Turn'][mask_Back], turn_data['count_backchannels'][mask_Back] * 0,
+    ax2.scatter(turn_level_data['Turn'][mask_Back], turn_level_data['count_backchannels'][mask_Back] * 0,
                  label='Backchannel',
-                 s = 70,
+                 s = 100,
                  marker = 'x',
                  color='#007F73')
     
-    mask_Over = turn_data['count_overlaps'] != 0
+    mask_Over = turn_level_data['mean_overlaps'] != 0
     
-    ax2.scatter(turn_data['Turn'][mask_Over], turn_data['count_overlaps'][mask_Over] * 0,
+    ax2.scatter(turn_level_data['Turn'][mask_Over], turn_level_data['mean_overlaps'][mask_Over] * 0,
                  label='Overlap',
                  marker = 'o',
-                 s = 50,
+                 s = 70,
                  facecolors='none',
                  color='#db1f48')
     
@@ -101,9 +120,16 @@ def plot_sentiment_time_series(turn_data):
     # Customize the plot
     # fig.title('Conversational Dynamics by Speaker', pad=100)
     ax2.set_xlabel('Turn', fontsize = 20)
-    ax1.set_ylabel('Word Count', fontsize = 20)
-    ax2.set_ylabel('Sentiment & \n Overlapping Speakers', fontsize = 20)
-    ax2.set_ylim(-1.2, 1.2)
+    ax1.set_ylabel('Word Count\n(log scale)', fontsize = 20)
+    ax1.set_yscale('log')
+    ax1.margins(x=0)
+    ax2.set_ylabel('Sentiment', fontsize = 20)
+    ax2.margins(x=0)
+    ax3.set_ylabel('Overlapping \nSpeakers', fontsize = 20)
+    ax3.set_yticklabels([])
+    ax3.set_yticks([])
+    ax3.margins(x=0)
+    # ax2.set_ylim(-1.2, 1.2)
     ax1.tick_params(axis='both', which='major', labelsize=20)
     ax2.tick_params(axis='both', which='major', labelsize=20)
     fig.legend(loc='upper center',
